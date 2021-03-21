8359a80da8cb6afa1f0b4236f45bd58180241b43
nikolayg92 <apokalipsys@mail.bg>
2021-03-21 14:37:10 +0200

    Database created for user service

---
+++ user-service/src/main/java/com/example/nuxel/userservice/model/entities/ProfileDetails.java
@@ -0,0 +1,30 @@
+package com.example.nuxel.userservice.model.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "profile_details")
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProfileDetails extends BaseEntity{
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
